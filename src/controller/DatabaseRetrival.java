9e328dfd78cbc1bf9e624f835ca98b7671c855bb
Kári Snær Kárason <kariskarason@gmail.com>
2017-03-23 17:00:46 +0000

    Bætti við gagnagrunns tengingunum

--- src/controller/DatabaseRetrival.java
+++ src/controller/DatabaseRetrival.java
@@ -1,5 +1,54 @@
 package controller;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class DatabaseRetrival {
+	public static void main(String[] argv) {
+
+		System.out.println("-------- PostgreSQL "
+				+ "JDBC Connection Testing ------------");
+
+		try {
+
+			Class.forName("org.postgresql.Driver");
+
+		} catch (ClassNotFoundException e) {
+
+			System.out.println("Where is your PostgreSQL JDBC Driver? "
+					+ "Include in your library path!");
+			e.printStackTrace();
+			return;
+
+		}
+
+		System.out.println("PostgreSQL JDBC Driver Registered!");
+
+		Connection connection = null;
+		
+		String URL = "jdbc:postgresql://kuldbinstance.czsqr6wtrtap.us-west-2.rds.amazonaws.com:5432/kuldb";
+		String USER = "kuluser";
+		String PASS = "kulpassword";
+		
+		try {
+
+			connection = DriverManager.getConnection(URL, USER, PASS);
+
+		} catch (SQLException e) {
+
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return;
+
+		}
+
+		if (connection != null) {
+			System.out.println("You made it, take control your database now!");
+		} else {
+			System.out.println("Failed to make connection!");
+		}
+		
+		
+	}
 
 }
