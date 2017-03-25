930d141dfb3ba6577b7eab04a9206595282379b2
stellarutg <stellarut95@gmail.com>
2017-03-25 18:24:21 +0000

    :)

--- src/controller/BookingController.java
+++ src/controller/BookingController.java
@@ -1,5 +1,38 @@
 package controller;
 
+import model.BookingModel;
+
 public class BookingController {
 
+	
+	public boolean bookTrip(String[] tripInfo) { // tripInfo er listi af tripId, bookerEmail, numPEople, bookerSSN
+		
+		boolean bookingComplete = false;
+		
+		
+		if(bookingComplete) {
+			sendVerification(tripInfo[1]);
+		}
+		
+		return bookingComplete;	
+	}
+	
+	public boolean sendVerification(String tripId) {
+		
+		boolean verificationSent;
+		
+		BookingModel[] tripBookings = DatabaseRetrival.queryTripBooking(tripId)
+				
+		for(int i=0; i < tripBookings.length; i++) {
+			contact(tripBookings[i].getBookerEmail());
+		}
+		
+		return verificationSent;
+	}
+	
+	public void contact(String email) {
+		
+		System.out.println(email);
+		
+	}
 }
--- src/model/BookingModel.java
+++ src/model/BookingModel.java
@@ -1,5 +1,38 @@
 package model;
 
 public class BookingModel {
-
+	
+	private String bookingId;
+	private String tripId;
+	private String bookerEmail;
+	private int numPeople;
+	private int bookerSSN;
+	
+	public BookingModel (String bookingId, String tripId, String bookerEmail, int numPeople, int bookerSSN) {
+		this.bookingId = bookingId;
+		this.tripId = tripId;
+		this.bookerEmail = bookerEmail;
+		this.numPeople = numPeople;
+		this.bookerSSN = bookerSSN;
+	}
+		
+	public String getBookingId() {
+		return bookingId;
+	}
+	
+	public String getTripId() {
+		return tripId;
+	}
+	
+	public String getBookerEmail() {
+		return bookerEmail;
+	}
+	
+	public int getNumPeople() {
+		return numPeople;
+	}
+	
+	public int getBookerSSN() {
+		return bookerSSN;
+	}
 }
