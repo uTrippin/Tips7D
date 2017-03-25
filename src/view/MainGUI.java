8e6d1359db8884c8a21ae753407caf939e6160f5
valgerdur <vas30@hi.is>
2017-03-25 17:50:56 +0000

    main

--- src/view/MainGUI.java
+++ src/view/MainGUI.java
@@ -7,9 +7,49 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import model.Trip;
+import model.SearchModel;
+import controller.Admin;
+import model.BookingModel;
+import javax.swing.JButton;
+
 public class MainGUI extends JFrame {
 
 	private JPanel contentPane;
+	private Trip tripList;
+	private SearchModel search;
+	private Admin adminConnection;
+	private BookingModel booker;
+	
+	
+	private Trip tripList(){
+		return tripList;
+	}
+	
+	private SearchModel search(){
+		return search;
+	}
+	
+	private Admin adminConnection(){
+		return adminConnection;
+	}
+	
+	private BookingModel booker(){
+		return booker;
+	}
+	
+	
+	private void showTrips(){
+		
+	}
+	
+	private void callSearch(){
+
+	}
+	
+	private void callBooking(){
+		
+	}
 
 	/**
 	 * Launch the application.
@@ -37,6 +77,14 @@ public class MainGUI extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		
+		JButton btnSearch = new JButton("Search");
+		panel.add(btnSearch);
 	}
+	
+	
 
 }
