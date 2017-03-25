5ea539b69cc90d24b19b3458518acb4c78ad204c
Kári Snær Kárason <kariskarason@gmail.com>
2017-03-25 17:09:11 +0000

    svona er þetta bara

--- src/controller/DatabaseRetrival.java
+++ src/controller/DatabaseRetrival.java
@@ -3,14 +3,18 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.SQLException;
 
+import model.SearchModel;
+import model.Trip;
+
 public class DatabaseRetrival {
+	
+	private static Connection connection = null; //Database connection
+	
 	public static void main(String[] argv) {
-
-		System.out.println("-------- PostgreSQL "
-				+ "JDBC Connection Testing ------------");
-
+		//Connect to the postgresql driver
 		try {
 
 			Class.forName("org.postgresql.Driver");
@@ -21,27 +25,20 @@ public class DatabaseRetrival {
 					+ "Include in your library path!");
 			e.printStackTrace();
 			return;
-
 		}
-
-		System.out.println("PostgreSQL JDBC Driver Registered!");
-
-		Connection connection = null;
 		
+		//Setting the database parameters
 		String URL = "jdbc:postgresql://kuldbinstance.czsqr6wtrtap.us-west-2.rds.amazonaws.com:5432/kuldb";
 		String USER = "kuluser";
 		String PASS = "kulpassword";
 		
+		//Connecting to the AWS database
 		try {
-
 			connection = DriverManager.getConnection(URL, USER, PASS);
-
 		} catch (SQLException e) {
-
 			System.out.println("Connection Failed! Check output console");
 			e.printStackTrace();
 			return;
-
 		}
 
 		if (connection != null) {
@@ -50,24 +47,73 @@ public class DatabaseRetrival {
 			System.out.println("Failed to make connection!");
 		}
 		
-		String selectSQL = "SELECT id, tester FROM TEST WHERE id = ?";
+		//query();
+		
+	}
+	
+	//Function for querying the database
+	public static Trip[] queryTrip(SearchModel search){
+		
+		Trip [] tripList;
+		//tripName
+		//dateBegin
+		//dateEnd
+		//location
+		//price
+		
+		String selectSQL = "SELECT * FROM TRIP WHERE tripName = ? AND dateBegin = ? AND dateEnd = ? AND location = ?"
+				+ "AND price = ?";
 		PreparedStatement preparedStatement;
 		try {
 			preparedStatement = connection.prepareStatement(selectSQL);
-			preparedStatement.setInt(1, 100);
+			preparedStatement.setString(1, search.getTripName());
+			preparedStatement.setDate(2, (Date) search.getDateBegin());
+			preparedStatement.setDate(3, (Date) search.getDateEnd());
+			preparedStatement.setString(4, search.getLocation());
+			preparedStatement.setInt(5, search.getPrice());
 			ResultSet rs = preparedStatement.executeQuery();
-			while (rs.next()) {
-				String userid = rs.getString("id");
-				String username = rs.getString("tester");
-				System.out.println(userid);
-				System.out.println(username);
-			}
+			tripList = createTriplist(rs);
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			tripList = new Trip[0];
 		}
-
 		
+		return tripList;
+		
+	}
+	
+	private static Trip[] createTriplist(ResultSet rs){
+		//Get the length of the results set
+		boolean b = rs.last();
+		int n = 0;
+		if(b){
+		    n = rs.getRow();
+		}
+		
+		//Create a list of Trip objects
+		Trip[] tripList = new Trip[n];
+		
+		int i = 0;
+		while (rs.next()) {
+			String tripName = rs.getString("tripName");
+			Date dateBegins = rs.getDate("dateBegin");
+			Date dateEnds = rs.getDate("dateEnd");
+			String location = rs.getString("location");
+			int price = rs.getInt("price");
+			tripList[i] = new Trip(tripName, dateBegins, dateEnds, location, price);
+			
+		}
+		
+		return tripList;
+	}
+	
+	public static void queryBooking(SearchModel search){
+		
+	}
+	
+	public static void queryAdmin(SearchModel search){
 		
 	}
 
