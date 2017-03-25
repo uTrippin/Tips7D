15d23a7e5cad96d0667ea7571d2c5a5b53da05a6
Kári Snær Kárason <kariskarason@gmail.com>
2017-03-25 20:46:15 +0000

    more db updates

--- src/controller/DatabaseUpdater.java
+++ src/controller/DatabaseUpdater.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import model.BookingModel;
+
 public class DatabaseUpdater {
 	private Connection connection = null;
 	private String URL = "jdbc:postgresql://kuldbinstance.czsqr6wtrtap.us-west-2.rds.amazonaws.com:5432/kuldb";
@@ -53,24 +55,27 @@ public class DatabaseUpdater {
 	}
 	
 	public static void main(String[] argv) {
-		
-		/*String insertTableSQL = "INSERT INTO TEST"
-				+ "(id, tester) VALUES"
-				+ "(?,?)";
+	
+	}
+	//Function that adds a booking to the table BOOKINGS
+	public void insertBooking(BookingModel booking){
+		String insertTableSQL = "INSERT INTO BOOKING"
+				+ "(tripId, bookerEmail, numPeople, bookerSSN) VALUES"
+				+ "(?,?,?,?)";
 		PreparedStatement preparedStatement;
 		try {
 			preparedStatement = connection.prepareStatement(insertTableSQL);
-			preparedStatement.setInt(1, 100);
-			preparedStatement.setString(2, "KariKul");
-			// execute insert SQL stetement
+			preparedStatement.setString(1, booking.getTripId());
+			preparedStatement.setString(2, booking.getBookerEmail());
+			preparedStatement.setInt(3, booking.getNumPeople());
+			preparedStatement.setInt(4, booking.getBookerSSN());
+			//execute insert SQL statement
 			preparedStatement .executeUpdate();
 			System.out.print("Awwww yeeeeaaaaaaaah");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}*/
-
-		
+		}
 	}
 	
 	public void createTables(){
@@ -78,31 +83,35 @@ public class DatabaseUpdater {
 		try{
 			stmt = connection.createStatement();
 		      
-		      String sql1 = "CREATE TABLE TRIP " +
-		                   "(tripId VARCHAR(256) NOT NULL, " +
-		                   " tripName VARCHAR(256) NOT NUll, " +
-		                   " dateBegin DATE NOT NULL, " +
-		                   " dateEnd DATE NOT NULL, " +
-		                   " description VARCHAR(2000), " +
-		                   " maxPeople INT NOT NULL, " +
-		                   " minPeople INT NOT NULL, " +
-		                   " location VARCHAR(256) NOT NULL, " +
-		                   " price INT NOT NULL)";
+			String sql0 = "DROP TABLE BOOKING;";
+			
+			stmt.executeUpdate(sql0);
+			
+		      /*String sql1 = "CREATE TABLE TRIP " +
+		                   	"(tripId VARCHAR(256) NOT NULL, " +
+		                   	" tripName VARCHAR(256) NOT NUll, " +
+		                   	" dateBegin DATE NOT NULL, " +
+		                   	" dateEnd DATE NOT NULL, " +
+		                   	" description VARCHAR(2000), " +
+		                   	" maxPeople INT NOT NULL, " +
+		                   	" minPeople INT NOT NULL, " +
+		                   	" location VARCHAR(256) NOT NULL, " +
+		                   	" price INT NOT NULL)";
 		                   
 		      stmt.executeUpdate(sql1);
 		      
 		      String sql2 = "CREATE TABLE ADMIN " +
-	                   "(adminId VARCHAR(256) NOT NULL, " +
-	                   " adminPassword VARCHAR(256) NOT NUll)";
+	                   		"(adminId VARCHAR(256) NOT NULL, " +
+	                   		" adminPassword VARCHAR(256) NOT NUll)";
 		      
-		      stmt.executeUpdate(sql2);
+		      stmt.executeUpdate(sql2);*/
 		      
 		      String sql3 = "CREATE TABLE BOOKING " +
-	                   "(bookingId VARCHAR(256) NOT NULL, " +
-	                   " tripId VARCHAR(256) NOT NUll, " +
-	                   "bookerEmail VARCHAR(256) NOT NULL, " + 
-	                   "numPeople INT NOT NULL, " +
-	                   "bookerSSN INT NOT NULL)";
+		    		  		"(bookingId VARCHAR(256) SERIAL PRIMARY KEY, " +
+		    		  		" tripId VARCHAR(256) NOT NUll, " +
+		    		  		"bookerEmail VARCHAR(256) NOT NULL, " + 
+		    		  		"numPeople INT NOT NULL, " +
+		    		  		"bookerSSN INT NOT NULL);";
 		      
 		      stmt.executeUpdate(sql3);
 		      
