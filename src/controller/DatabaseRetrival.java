e635319f00e5c23ac4f5bafc121307aa560a51a6
stellarutg <stellarut95@gmail.com>
2017-04-08 15:28:57 +0000

    counter breyta í trip töflu

--- src/controller/DatabaseRetrival.java
+++ src/controller/DatabaseRetrival.java
@@ -21,6 +21,9 @@ public class DatabaseRetrival {
 			Class.forName("org.postgresql.Driver");
 
 		} catch (ClassNotFoundException e) {
+
+			//System.out.println("Where is your PostgreSQL JDBC Driver? "
+			//		+ "Include in your library path!");
 			e.printStackTrace();
 			return;
 		}
@@ -34,8 +37,15 @@ public class DatabaseRetrival {
 		try {
 			connection = DriverManager.getConnection(URL, USER, PASS);
 		} catch (SQLException e) {
+			//System.out.println("Connection Failed! Check output console");
 			e.printStackTrace();
 			return;
+		}
+
+		if (connection != null) {
+			//System.out.println("You made it, take control of your database now!");
+		} else {
+			//System.out.println("Failed to make connection!");
 		}
 	}
 	
@@ -120,8 +130,9 @@ public class DatabaseRetrival {
 				String location = rs.getString("location");
 				int price = rs.getInt("price");
 				int tripId = rs.getInt("tripId");
+				int numBooking = rs.getInt("numBooking");
 				
-				tripList[i] = new Trip(tripName, dateBegins, dateEnds, desc, maxPeople, minPeople, location, price, tripId);
+				tripList[i] = new Trip(tripName, dateBegins, dateEnds, desc, maxPeople, minPeople, location, price, tripId, numBooking);
 				i++;
 			}
 		} catch(SQLException e){
@@ -237,17 +248,17 @@ public class DatabaseRetrival {
 	}
 	
 	//test function
-	public String[] simpleQuery() {
-		String s[] = new String[100];
+	public String simpleQuery() {
+		String s = "";
 		String selectSQL = "SELECT * FROM BOOKING";
 		PreparedStatement preparedStatement;
 		try {
 			preparedStatement = connection.prepareStatement(selectSQL);
 			ResultSet rs = preparedStatement.executeQuery();
-			int i = 0;
+			
 			while (rs.next()) {
-				s[i] = rs.getString("bookerSSN") + " " + rs.getString("tripId") + " "  + rs.getString("bookerEmail") + " "  + rs.getString("numPeople");
-				i++;
+				s = rs.getString("tripId") + "\t" + rs.getString("bookerEmail") + "\t" + rs.getString("numPeople");
+				System.out.println(s);
 			}
 			
 		} catch (SQLException e) {
